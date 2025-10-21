10cddee286e5dc1637d9748e2a6912dd3ffb7989
Moshe Dicker <dickermoshe@gmail.com>
2025-10-21 13:41:34 -0400

    more bindings

--- src/value/json/bytes.rs
+++ src/value/json/bytes.rs
@@ -1,13 +1,17 @@
+use flutter_rust_bridge::frb;
+
 /// Helper functions for encoding `Bytes`s as `String`s.
 pub enum JsonBytes {}
 
 impl JsonBytes {
     /// Encode a binary string as a string.
+    #[frb(sync)]
     pub fn encode(bytes: &Vec<u8>) -> String {
         base64::encode(&bytes[..])
     }
 
     /// Decode a binary string from a string.
+    #[frb(sync)]
     pub fn decode(s: String) -> anyhow::Result<Vec<u8>> {
         Ok(base64::decode(s.as_bytes())?)
     }
